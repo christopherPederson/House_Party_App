cb5bcf399bbeeb9b74a0e5672382774e6a7ab0fe
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2024-03-18 20:22:14 -0400

    included frontend URLs

---
+++ music_controller/frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+export default function App(){
+    return(
+        <h1>React Code</h1>
+    )
+}
+
+const appDiv = document.getElementById("app");
